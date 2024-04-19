083beef8699baa8f191b1823b6ca03721b24995a
budindepunk <carolinasilvarode@gmail.com>
2024-04-19 09:04:47 +0200

    more

---
+++ ft_strrchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csilva-r <csilva-r@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 08:29:11 by csilva-r          #+#    #+#             */
+/*   Updated: 2024/04/19 09:01:00 by csilva-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+	char	*hold;
+
+	i = 0;
+	hold = NULL;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			hold = (char *)(s + i);
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)(s + i));
+	return (hold);
+}
