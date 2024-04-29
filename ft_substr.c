ae69d50ea0508789718c2c27a568f0f515cefd47
budindepunk <carolinasilvarode@gmail.com>
2024-04-30 00:00:19 +0200

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,32 +6,32 @@
 /*   By: csilva-r <csilva-r@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 15:12:22 by csilva-r          #+#    #+#             */
-/*   Updated: 2024/04/29 22:52:11 by csilva-r         ###   ########.fr       */
+/*   Updated: 2024/04/30 00:00:05 by csilva-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*new;
 	size_t	i;
-	size_t	j;
 
-	new = (char *)malloc(sizeof(*s) * (len + 1));
+	new = (char *)malloc(sizeof(char) * (len + 1));
 	if (!new)
 		return (NULL);
-	i = 0;
-	j = 0;
-	while (s[i])
+	if (!s || (size_t)start >= ft_strlen(s) || start < 0)
 	{
-		if (i >= start && j < len)
-		{
-			new[j] = new[i];
-			j++;
-		}
+		new[0] = '\0';
+		return (new);
+	}
+	i = 0;
+	while (i < len)
+	{
+		new[i] = s[start + i];
 		i++;
 	}
-	new[j] = '\0';
+	new[i] = '\0';
 	return (new);
 }
