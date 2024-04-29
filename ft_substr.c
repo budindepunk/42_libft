65d17759fbc554da004e7f14eb5b78ee831057e8
budindepunk <carolinasilvarode@gmail.com>
2024-04-30 01:26:40 +0200

    la gaucheada de matthias

--- ft_substr.c
+++ ft_substr.c
@@ -6,26 +6,37 @@
 /*   By: csilva-r <csilva-r@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 15:12:22 by csilva-r          #+#    #+#             */
-/*   Updated: 2024/04/30 00:00:05 by csilva-r         ###   ########.fr       */
+/*   Updated: 2024/04/30 01:24:33 by csilva-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+
+static char	*empty_string(void)
+{
+	char	*new;
+
+	new = malloc(sizeof(char));
+	if (!new)
+		return (NULL);
+	new[0] = '\0';
+	return (new);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*new;
 	size_t	i;
 
-	new = (char *)malloc(sizeof(char) * (len + 1));
+	if (s == NULL)
+		return (NULL);
+	if (ft_strlen(s) < start)
+		return (empty_string());
+	if (start + len > ft_strlen(s))
+		len = ft_strlen(s) - start;
+	new = malloc(sizeof(char) * (len + 1));
 	if (!new)
 		return (NULL);
-	if (!s || (size_t)start >= ft_strlen(s) || start < 0)
-	{
-		new[0] = '\0';
-		return (new);
-	}
 	i = 0;
 	while (i < len)
 	{
